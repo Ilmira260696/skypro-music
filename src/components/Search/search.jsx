73c3634d6aceab9dd9ad9c6baf941fe83d16123b
Ilmira260696 <130978593+Ilmira260696@users.noreply.github.com>
2023-09-11 13:29:30 +0400

    разбивка на компоненты

---
+++ src/components/Search/search.jsx
@@ -0,0 +1,19 @@
+import "./search.css"
+function Seach() {
+    return(
+        <div className="main__centerBlock centerBlock">
+        <div className="centerBlock__search search">
+          <svg className="search__svg">
+            <use xlinkHref="public/img/icon/sprite.svg#icon-search"></use>
+          </svg>
+          <input
+            className="search__text"
+            type="search"
+            placeholder="Поиск"
+            name="search"
+          />
+        </div> 
+        </div>  
+    )  
+}
+export default Seach;
---
+++ src/components/Sidebar/sidebar.jsx
@@ -0,0 +1,44 @@
+import "./sidebar.css"
+function SideBar (){
+    return(
+        <div className="main__sidebar sidebar">
+        <div className="sidebar__personal">
+          <p className="sidebar__personal-name">Sergey.Ivanov</p>
+          <div className="sidebar__icon">
+            <svg alt="logout">
+              <use xlinkHref="img/icon/sprite.svg#logout"></use>
+            </svg>
+          </div>
+        </div>
+        <div className="sidebar__block">
+          <div className="sidebar__list">
+            <div className="sidebar__item">
+              <button className="sidebar__link" href="#"></button>
+              <img
+                className="sidebar__img"
+                src="../img/playlist01.png"
+                alt="day's playlist"
+              />
+            </div>
+            <div className="sidebar__item">
+              <button className="sidebar__link" href="#"></button>
+              <img
+                className="sidebar__img"
+                src="../img/playlist02.png"
+                alt="day's playlist"
+              />
+            </div>
+            <div className="sidebar__item">
+              <button className="sidebar__link" href="#"></button>
+              <img
+                className="sidebar__img"
+                src="../img/playlist03.png"
+                alt="day's playlist"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
+export default SideBar;
