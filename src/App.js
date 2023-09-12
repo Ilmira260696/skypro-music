84c93a8c0f6bf7ddfd4cabec503fc8b82dcb0ce8
Ilmira260696 <130978593+Ilmira260696@users.noreply.github.com>
2023-09-12 11:18:08 +0400

    изменения2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Filters from "./components/Filters/Filters";
+import Search from "./components/Search/Search"
+import TrackList from "./components/TrackList/TrackList";
+import SideBar from "./components/Sidebar/Sidebar";
+import AudioPlayer from "./components/AudioPlayer/AudioPlayer";
+import NavMenu from "./components/NavMenu/NavMenu";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wrapper">
+        <div className="container">
+          <main className="main">
+          <NavMenu/>
+            <div className="main__centerBlock centerBlock">
+          <Search/>
+
+              <h2 className="centerBlock__h2">Треки</h2>
+              <Filters />
+              <TrackList />
+            </div>
+            <SideBar />
+          </main>
+
+          <AudioPlayer />
+        </div>
+        <footer className="footer"></footer>
+      </div>
     </div>
   );
 }
