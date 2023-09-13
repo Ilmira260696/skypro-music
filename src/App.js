2292055246ce43d87271534ff20841f33ba180a0
Ilmira260696 <130978593+Ilmira260696@users.noreply.github.com>
2023-09-13 18:08:21 +0400

    filters.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import React from "react";
-import Filters from "./components/Filters/Filters";
+
 import Search from "./components/Search/Search";
 import TrackList from "./components/TrackList/TrackList";
 import SideBar from "./components/Sidebar/Sidebar";
 import AudioPlayer from "./components/AudioPlayer/AudioPlayer";
 import NavMenu from "./components/NavMenu/NavMenu";
+import Filters from "./components/Filters/Filters";
 
 function App() {
   return (
@@ -16,9 +17,9 @@ function App() {
             <NavMenu />
             <div className="main__centerBlock centerBlock">
               <Search />
-
+<Filters/>
               <h2 className="centerBlock__h2">Треки</h2>
-              <Filters />
+            
               <TrackList />
             </div>
             <SideBar />
