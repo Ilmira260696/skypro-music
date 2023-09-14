234cc0bf2e2076bf875dd35887d4f214442da764
Ilmira260696 <130978593+Ilmira260696@users.noreply.github.com>
2023-09-14 11:05:35 +0400

    бургер-меню при клике скрывается

--- src/components/NavMenu/NavMenu.jsx
+++ src/components/NavMenu/NavMenu.jsx
@@ -1,17 +1,22 @@
 import "./NavMenu.css";
 import React from "react";
+import {useState} from "react";
 
 function NavMenu() {
+  const [visible, setVisible] = useState(true);
+  const toggleVisibility = () => setVisible(!visible);
   return (
     <nav className="main__nav nav">
       <div className="nav__logo logo">
-        <img className="logo__image" src="img/logo.png" alt="logo" />
+        <img  className="logo__image" src="img/logo.png" alt="logo" />
       </div>
-      <div className="nav__burger burger">
+ 
+      <div onClick={toggleVisibility} className="nav__burger burger">
         <span className="burger__line"></span>
         <span className="burger__line"></span>
         <span className="burger__line"></span>
       </div>
+      {visible && (
       <div className="nav__menu menu">
         <ul className="menu__list">
           <li className="menu__item">
@@ -31,8 +36,11 @@ function NavMenu() {
             </a>
           </li>
         </ul>
+        
       </div>
+       )}
     </nav>
-  );
-}
+    )}
+
+
 export default NavMenu;
