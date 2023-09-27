ce3c08a30fb0e183282ff92faa4ae74499e5ff6b
Ilmira260696 <130978593+Ilmira260696@users.noreply.github.com>
2023-09-27 16:31:25 +0400

    добавила импорт react в Main Page

--- src/Pages/Main/MainPage.jsx
+++ src/Pages/Main/MainPage.jsx
@@ -1,5 +1,6 @@
-import { useState, useEffect } from "react";
+import React from "react";
 import * as S from "./MainPageStyle";
+import { useState, useEffect } from "react";
 import Search from "../../components/Search/Search";
 import TrackList from "../../components/TrackList/TrackList";
 import SideBar from "../../components/Sidebar/Sidebar";
