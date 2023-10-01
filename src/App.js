34ac163e2551b304452b286a00c4098522a9d6a9
Ilmira260696 <130978593+Ilmira260696@users.noreply.github.com>
2023-10-01 20:46:55 +0400

    фин изменения

--- src/App.js
+++ src/App.js
@@ -3,19 +3,13 @@ import { AppRoutes } from "./routes";
 import "./index.css";
 import { useState } from "react";
 
-
-
 function App() {
-
   const [user, setUser] = useState(false);
-
-
   const handleLogin = () => {
     localStorage.setItem('user', 'true');
     const curentLocalStorage = localStorage.getItem('user');
     console.log(curentLocalStorage);
     setUser(curentLocalStorage);
-  
   }
   return (
     <div className="App">
@@ -24,8 +18,6 @@ function App() {
      onAuthButtonClick={handleLogin}
      />
            </div>
-
-   
   )
  }
   export default App;
