65200e6104f9ce6f248a9c9c238a1f0cdc2ad39f
Ilmira260696 <130978593+Ilmira260696@users.noreply.github.com>
2023-10-05 07:48:34 +0400

    переименование файлов

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Main from "./Pages/main/mainPage";
-import { Favorites } from "./Pages/favorites/favorites";
+import Main from "./Pages/Main/mainPage";
+import { Favorites } from "./Pages/Favorites/favorites";
 import { Category } from "./Pages/category/category";
 import { NotFound } from "./Pages/NotFound/NotFound";
-import { Login } from "./Pages/login/login";
-import { Registration } from "./Pages/registration/registration";
+import { Login } from "./Pages/Login/login";
+import { Registration } from "./Pages/Registration/registration";
 import { ProtectedRoute } from "./ProtectedRoute/ProtectedRoute";
 
 export function AppRoutes({ user, onAuthButtonClick }) {
