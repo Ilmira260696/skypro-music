5eb4ff77bcec5acfe3a9ac3b871f3212fa84d325
Ilmira260696 <130978593+Ilmira260696@users.noreply.github.com>
2023-10-05 08:18:47 +0400

    проект открывается

--- src/components/TrackList/TrackList.jsx
+++ src/components/TrackList/TrackList.jsx
@@ -1,67 +1,67 @@
-import React, { useEffect, useState } from "react";
-import * as S from "./TrackListStyle";
-import { GetAllTracks } from "../../Api";
-import ArrTracks from "../../ArrTracks/ArrTracks";
+import React from "react";
+import *as S  from "./TrackListStyle"
+import ArrTracks from "../../ArrTracks/ArrTracks"
 
 
-function Tracks({ loading, tracks}) {
-  
-  const trackItems = tracks.map((track) => (
-    <S.PlaylistItem   key={track.id} >
-      try {
-      <S.PlaylistTrack>
-        <S.TrackTitle>
-          <S.TrackTitleImg>
-            <S.TrackTitleSvg alt="music">
-              <use xlinkHref="img/icon/sprite.svg#icon-note"></use>
-            </S.TrackTitleSvg>
-          </S.TrackTitleImg>
-          {loading ? (
-            <div className="track__title-text">
-              <S.TrackListTitleLink href="http://">
-                {track.name}
-                {track.remix ? (
+function Tracks({loading}) {
+  const trackItems = ArrTracks.map((track) => (
+        <S.PlaylistItem key={track.id} className="playlist__item">
+          <S.PlaylistTrack>
+            <S.TrackTitle>
+             <S.TrackTitleImg>
+                <S.TrackTitleSvg alt="music">
+                  <use xlinkHref="img/icon/sprite.svg#icon-note"></use>
+                </S.TrackTitleSvg>
+                </S.TrackTitleImg>
+              {loading ? (
+              <div className="track__title-text">
+                <S.TrackListTitleLink href="http://">
+                 {track.trackName}
+                 {track.remix ? (
                   <S.TrackTitleSpan>({track.remix})</S.TrackTitleSpan>
-                ) : (
-                  ""
-                )}
-              </S.TrackListTitleLink>
-            </div>
-          ) : (
-            <S.Skeleton> </S.Skeleton>
-          )}
-        </S.TrackTitle>
-        {loading ? (
+                  ) : (
+                    ""
+                  )}
+                </S.TrackListTitleLink>
+              </div>
+               ) : (
+                <S.Skeleton> </S.Skeleton>
+              )}
+           </S.TrackTitle>
+            {loading ? (
           <S.TrackAuthor>
-            <S.TrackAuthorLink href="http://">{track.author}</S.TrackAuthorLink>
-          </S.TrackAuthor>
-        ) : (
-          <S.SkeletonAuthor> </S.SkeletonAuthor>
-        )}
+              <S.TrackAuthorLink href="http://">
+              {track.trackAuthor}
+              </S.TrackAuthorLink>
+              </S.TrackAuthor>
+                ) : (
+                  <S.SkeletonAuthor> </S.SkeletonAuthor>)}
+              
 
-        {loading ? (
-          <S.TrackAlbum>
-            <S.TrackAlbumLink href="http://">{track.album}</S.TrackAlbumLink>
-          </S.TrackAlbum>
-        ) : (
-          <S.SkeletonAlbum> </S.SkeletonAlbum>
-        )}
-
-        <div className="track__time">
-          <S.TrackTimeSvg alt="time">
-            <use xlinkHref="img/icon/sprite.svg#icon-like"></use>
-          </S.TrackTimeSvg>
-          <S.TrackTimeText>{track.release_date}</S.TrackTimeText>
-        </div>
-      </S.PlaylistTrack>
-} catch (error) {
-
-}
-    </S.PlaylistItem>
-  ));
-  return (
-  <S.ContentPlaylist>{trackItems}</S.ContentPlaylist>
-  )
-}
-
-export default Tracks;
+            {loading ? (
+           <S.TrackAlbum>
+              <S.TrackAlbumLink href="http://">
+               {track.trackAlbum}
+              </S.TrackAlbumLink>
+              </S.TrackAlbum>
+            ) : 
+              (<S.SkeletonAlbum> </S.SkeletonAlbum>)}
+           
+          
+            <div className="track__time">
+              <S.TrackTimeSvg alt="time">
+                <use xlinkHref="img/icon/sprite.svg#icon-like"></use>
+              </S.TrackTimeSvg>
+              <S.TrackTimeText>{track.trackTime}</S.TrackTimeText>
+            </div>
+             
+            </S.PlaylistTrack>
+      </S.PlaylistItem>
+          )
+          );
+          
+          return <S.ContentPlaylist>{trackItems}</S.ContentPlaylist>;
+          
+        }
+     
+      export default Tracks;
