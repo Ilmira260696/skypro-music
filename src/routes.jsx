b61c66dd98aec2fb8e46d0d76441268ee817b75e
Ilmira260696 <130978593+Ilmira260696@users.noreply.github.com>
2023-10-05 07:59:09 +0400

    Update routes.jsx

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Main from "./Pages/main/mainPage";
-import { Favorites } from "./Pages/favorites/favorites";
-import { Category } from "./Pages/category/category";
+import { Favorites } from "./Pages/Favorites/favorites";
+import { Category } from "./Pages/Category/category";
 import { NotFound } from "./Pages/NotFound/NotFound";
-import { Login } from "./Pages/login/login";
-import { Registration } from "./Pages/registration/registration";
+import { Login } from "./Pages/Login/login";
+import { Registration } from "./Pages/Registration/registration";
 import { ProtectedRoute } from "./ProtectedRoute/ProtectedRoute";
 
 export function AppRoutes({ user, onAuthButtonClick }) {
